0c2c2f743f0ffd50371e5c5f22533d6b6c2c76e6
Sam Decrock <sam.decrock@gmail.com>
2014-06-10 15:33:00 +0200

    basic express4 webserver

---
+++ webapp/app.js
@@ -0,0 +1,69 @@
+#!/usr/bin/env node
+
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+
+// EXPRESS: BASE SETUP
+// ==============================================
+var app     = express();
+var port    = process.env.PORT || 3000;
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(logger('tiny'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(require('stylus').middleware(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// EXPRESS: ROUTES
+// ==============================================
+
+app.get('/', function (req, res){
+	res.render('index', { title: 'Hello World' });
+});
+
+
+// EXPRESS: ERROR HANDLING
+// ==============================================
+/// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+// EXPRESS: START THE SERVER
+// ==============================================
+var webserver = app.listen(port);
+console.log('Express server listening on port ' + port);
+
+
+
+
+
